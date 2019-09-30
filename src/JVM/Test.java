6a4b8992614c560c415644211a74f0ab4aae3052
LisCodee <1376417539@qq.com>
2019-09-30 21:08:43 +0800

    内部类

---
+++ src/JVM/Test.java
@@ -0,0 +1,16 @@
+package JVM;
+
+public class Test {
+    public int a = 3;
+    static Integer si = 6;
+    String s = "Hello world!";
+
+    public static void main(String[] args) {
+        Test test = new Test();
+        test.a = 8;
+        si = 9;
+    }
+    private void test(){
+        this.a = a;
+    }
+}
