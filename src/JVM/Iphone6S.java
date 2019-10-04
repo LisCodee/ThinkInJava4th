89d1a721aea34fff735cfad38b3e510144fad17d
LisCodee <1376417539@qq.com>
2019-10-04 21:15:44 +0800

    并发

---
+++ src/JVM/Iphone6S.java
@@ -0,0 +1,11 @@
+package JVM;
+
+public class Iphone6S {
+    int length = 138;
+    int width = 67;
+    int height = 7;
+    int weight = 142;
+    int ram = 2;
+    int rom = 16;
+    int pixel = 1200;
+}
