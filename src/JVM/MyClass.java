08a4dd462b50a4b69ea648d1c1b58e7f8812ed10
LisCodee <1376417539@qq.com>
2019-10-05 11:36:26 +0800

    类变量解析

---
+++ src/JVM/MyClass.java
@@ -0,0 +1,8 @@
+package JVM;
+
+public abstract class MyClass {
+    private Integer i = 1;
+    protected long plong = 12L;
+    protected final short s = 6;
+    public char c = 'A';
+}
--- src/JVM/Test.java
+++ src/JVM/Test.java
@@ -1,16 +1,20 @@
 package JVM;
+import JVM.MyClass;
 
-public class Test {
-    public int a = 3;
-    static Integer si = 6;
-    String s = "Hello world!";
+public class Test extends MyClass {
+    private long l;
+    private Integer i = 3;
+    private long plong = 18L;
+    public char c = 'B';
+    public void add(int a, int b){
+        Test test = this;
+        int z = a+b;
+        int x = 3;
+    }
 
     public static void main(String[] args) {
         Test test = new Test();
-        test.a = 8;
-        si = 9;
-    }
-    private void test(){
-        this.a = a;
+        test.add(2,3);
+        System.exit(0);
     }
 }
