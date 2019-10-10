99476ee167507db5838aeadfa036d4e7ca48340a
LisCodee <1376417539@qq.com>
2019-10-10 22:59:25 +0800

    多线程

---
+++ src/测试开发/Test.java
@@ -0,0 +1,13 @@
+package 测试开发;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class Test{
+    public static void main(String[] args) {
+        ExecutorService es = Executors.newCachedThreadPool();
+
+    }
+}
